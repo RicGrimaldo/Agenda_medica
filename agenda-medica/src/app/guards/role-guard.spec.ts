128d715073ed43769ab97c2ace38c4c4c5ba4bf4
José Fernando Chan Dzib <56355924+Jfeerchan23@users.noreply.github.com>
2023-11-25 20:50:44 -0600

    se eliminaron los archivos spect.ts

--- agenda-medica/src/app/guards/role-guard.spec.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from '@angular/router';
-import { RoleGuard } from './role-guard';
-import { inject } from '@angular/core';
-
-describe('RoleGuard', () => {
-  it('should create an instance', () => {
-    expect(new RoleGuard(inject(Router))).toBeDefined();
-  });
-});
