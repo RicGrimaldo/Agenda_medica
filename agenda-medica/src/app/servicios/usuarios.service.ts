cdae17e821a1325fe640cd053c2d03271d5c7e5c
José Fernando Chan Dzib <56355924+Jfeerchan23@users.noreply.github.com>
2023-11-30 20:30:17 -0600

    eslint servicios finalizado

--- agenda-medica/src/app/servicios/usuarios.service.ts
+++ agenda-medica/src/app/servicios/usuarios.service.ts
@@ -1,20 +1,22 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { PeticionService } from './peticion.service';
+import { Injectable } from '@angular/core'
+import { Observable } from 'rxjs'
+import { PeticionService } from './peticion.service'
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
-  private URL = "http://localhost:8080";
-  constructor(
-    private peticionService: PeticionService
+  private readonly URL = 'http://localhost:8080'
+  constructor (
+    private readonly peticionService: PeticionService
   ) {
   }
-  obtenerTodosUsuarios(): Observable<any> {
-    return this.peticionService.getRequest(`${this.URL}/api/informacion/usuarios`);
+
+  obtenerTodosUsuarios (): Observable<any> {
+    return this.peticionService.getRequest(`${this.URL}/api/informacion/usuarios`)
   }
-  login(data: any): Observable<any> {
-    return this.peticionService.postRequest(`${this.URL}/api/login/auth`, data);
+
+  login (data: any): Observable<any> {
+    return this.peticionService.postRequest(`${this.URL}/api/login/auth`, data)
   }
 }
